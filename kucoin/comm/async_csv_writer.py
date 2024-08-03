612087be28862cdc73121a3264b596983ea788cc
YingjieHit <258349394@qq.com>
2024-08-03 20:05:51 +0800

    添加异步csv接口

---
+++ kucoin/comm/async_csv_writer.py
@@ -0,0 +1,25 @@
+import os
+import aiofiles
+import csv
+
+class AsyncCsvWriter:
+    def __init__(self):
+        pass
+
+    async def write_header_if_needed(self, file_name, header):
+        if not os.path.exists(file_name):
+            async with aiofiles.open(file_name, mode='w', newline='') as f:
+                writer = csv.writer(f)
+                await writer.writerow(header)
+
+    async def write_row(self, data, file_name):
+        async with aiofiles.open(file_name, mode='a', newline='') as f:
+            writer = csv.writer(f)
+            await writer.writerow(data)
+
+    async def remove_file(self, file_name):
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+
+
